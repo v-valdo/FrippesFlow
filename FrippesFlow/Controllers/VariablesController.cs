ee3572d3911f9e59c62c3b6b4440acdecab1e0e5
NL <elias.nimlandlind@elev.ga.lbs.se>
2024-10-08 15:56:21 +0200

    Added view to change variable values.

---
+++ FrippesFlow/Controllers/VariablesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+namespace FrippesFlow.Controllers
+{
+    public class VariablesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
