0bc390011e3efe131e52662094123b914fa269d3
ericbr0wne <ericbrowne@hotmail.com>
2024-10-08 11:27:41 +0200

    HttpGet

--- FrippesFlow/Controllers/SalesController.cs
+++ FrippesFlow/Controllers/SalesController.cs
@@ -1,12 +1,34 @@
+using FrippesFlow.data;
 using Microsoft.AspNetCore.Mvc;
 namespace FrippesFlow.Controllers
 {
+
+    [Route("sales")]
     public class SalesController : Controller
     {
+        private readonly FrippesFlowContext _context;
+        public SalesController(FrippesFlowContext context)
+        {
+          _context = context;
+        }
+       
+       //Read Result
+        [HttpGet]
         public ActionResult Index()
         {
+           var sales = _context.SalesEntries.ToList();
+
             return View();
         }
+
+        //Read Monthly expense
+
+        //Read Production costs
+
+        //Read IngredientPer10k
+
+        //Create sales per week => SalesEntry
+        
 
     }
 }
